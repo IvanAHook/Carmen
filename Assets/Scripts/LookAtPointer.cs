a1ced3123013d5191d73cca765f07353fb3f99a5
Ivan Höök <ivan.a.hook@gmail.com>
2016-02-25 14:19:19 +0100

    Initial commit

---
+++ Assets/Scripts/LookAtPointer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtPointer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	}
+}
