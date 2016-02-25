2212b85a24ee25c5a2b52423a895645172c47977
Ivan Höök <ivan.a.hook@gmail.com>
2016-02-25 16:59:16 +0100

    starting on movement

--- Assets/Scripts/LookAtPointer.cs
+++ Assets/Scripts/LookAtPointer.cs
@@ -3,13 +3,38 @@ using System.Collections;
 
 public class LookAtPointer : MonoBehaviour {
 
-	// Use this for initialization
+	private const float BASESPEED = 5f;
+
 	void Start () {
-	
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector3 lookAtPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector3 mousePoint = new Vector3(lookAtPoint.x, lookAtPoint.y, 0);
+
+		float angle = GetAngle(transform.position, mousePoint);
+		transform.eulerAngles = new Vector3(0, 0, angle);
+
+		Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 1);
+	}
+
+	private void Move(float horizontal, float vertical, float speedModifier) // direction vector normalized for consitent movement speed ples
+	{
+		float speed = BASESPEED * speedModifier;
+
+		Vector2 movement = new Vector3(
+			transform.position.x + horizontal * speed * Time.deltaTime,
+			transform.position.y + vertical * speed * Time.deltaTime,
+			0.0f);
+
+		transform.position = movement;
+	}
+
+	private float GetAngle(Vector3 currentLocation, Vector3 mouseLocation)
+	{
+		float x = mouseLocation.x - currentLocation.x;
+		float y = mouseLocation.y - currentLocation.y;
+
+		return Mathf.Rad2Deg * Mathf.Atan2(y, x) - 90;
 	}
 }
