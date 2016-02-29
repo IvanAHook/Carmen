d8a9d6ae495555edf676def4c6b1bf7d82e191e8
Ivan Höök <ivan.a.hook@gmail.com>
2016-02-29 18:54:27 +0100

    lala

--- Assets/Scripts/LookAtPointer.cs
+++ Assets/Scripts/LookAtPointer.cs
@@ -3,32 +3,68 @@ using System.Collections;
 
 public class LookAtPointer : MonoBehaviour {
 
-	private const float BASESPEED = 5f;
+	private const float BASESPEED = 2f;
 
-	void Start () {
+	private CharacterController cc;
+
+	void Awake()
+	{
+		cc = GetComponent<CharacterController>();
 	}
 	
-	void Update () {
+	void Update()
+	{
 		Vector3 lookAtPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		Vector3 mousePoint = new Vector3(lookAtPoint.x, lookAtPoint.y, 0);
 
 		float angle = GetAngle(transform.position, mousePoint);
 		transform.eulerAngles = new Vector3(0, 0, angle);
 
-		Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 1);
+		PhysicsMove();
+		//Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 1);
 	}
 
-	private void Move(float horizontal, float vertical, float speedModifier) // direction vector normalized for consitent movement speed ples
+	private void PhysicsMove()
 	{
-		float speed = BASESPEED * speedModifier;
+		Vector2 direction = Vector2.zero;
+		//Left
+		if (Input.GetKey(KeyCode.A))
+		{
+			direction += Vector2.left;
+		}
+		//Right
+		if(Input.GetKey(KeyCode.D)){
+			direction += Vector2.right;
+		}
+		//Up
+		if (Input.GetKey(KeyCode.W)){
+			direction += Vector2.up;
+		}
+		//Down
+		if(Input.GetKey(KeyCode.S)){
+			direction += Vector2.down;
+		}
 
-		Vector2 movement = new Vector3(
-			transform.position.x + horizontal * speed * Time.deltaTime,
-			transform.position.y + vertical * speed * Time.deltaTime,
-			0.0f);
-
-		transform.position = movement;
+		if (direction != Vector2.zero)
+		{
+			cc.Move(direction.normalized * BASESPEED * Time.deltaTime);
+			//cc.SimpleMove(direction.normalized * BASESPEED * Time.deltaTime);
+			//rb2d.AddForce(direction.normalized * BASESPEED * Time.deltaTime);
+		}
+			
 	}
+
+	//private void Move(float horizontal, float vertical, float speedModifier) // direction vector normalized for consitent movement speed ples
+	//{
+	//	float speed = BASESPEED * speedModifier;
+
+	//	Vector2 movement = new Vector3(
+	//		transform.position.x + horizontal * speed * Time.deltaTime,
+	//		transform.position.y + vertical * speed * Time.deltaTime,
+	//		0.0f);
+
+	//	transform.position = movement;
+	//}
 
 	private float GetAngle(Vector3 currentLocation, Vector3 mouseLocation)
 	{
@@ -37,4 +73,5 @@ public class LookAtPointer : MonoBehaviour {
 
 		return Mathf.Rad2Deg * Mathf.Atan2(y, x) - 90;
 	}
+
 }
